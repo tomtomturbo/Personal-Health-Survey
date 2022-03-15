3a574d669e1b924ae4192935a952d09555d2bf05
tomtomturbo <thomasreist@gmail.com>
2022-03-15 16:59:55 +0100

    test

---
+++ inc/header.php
@@ -0,0 +1,15 @@
+
+
+
+<div class="container" style="background-color:blueviolet; padding:20px 10px">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link nav-link-thomas" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Previous</a></li></a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Next</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+      </ul>
+    </header>
+  </div>
+
+  
